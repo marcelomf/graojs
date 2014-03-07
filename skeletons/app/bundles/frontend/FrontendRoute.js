7ef25dfd3d5d42960be20cffcfa2d6429d79c22e
Marcelo M. Fleury <marcelomf@gmail.com>
2014-03-07 14:03:24 -0300

    Add handle errors

--- skeletons/app/bundles/frontend/FrontendRoute.js
+++ skeletons/app/bundles/frontend/FrontendRoute.js
@@ -10,6 +10,26 @@ var FrontendRoute = function (di) {
     res.redirect("/");
   });
 
+  di.graoExpress.use(function(req, res, next){
+    res.status(404);
+    if (req.accepts('html')) {
+      res.render('frontend/theme/404', { url: req.url });
+      return;
+    }
+
+    if (req.accepts('json')) {
+      res.json(event.new('Not Found...').error().log('error').toJson());
+      return;
+    }
+
+    res.type('txt').send('Not found');
+  });
+
+  di.graoExpress.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('frontend/theme/500', { error: err });
+  });
+
   di.graoExpress.post('/login', di.controllers.passport.service.login);
   di.graoExpress.get('/logout', di.controllers.passport.service.logout);
 };
