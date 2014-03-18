b437c428e4099babb1b2beac0fd046e7921de253
Marcelo M. Fleury <marcelomf@gmail.com>
2014-03-18 11:32:26 -0300

    Add /fonts to fix ui date bug

--- skeletons/app/bundles/frontend/config.js
+++ skeletons/app/bundles/frontend/config.js
@@ -9,6 +9,7 @@ module.exports = exports = {
     { fsdir: "/bundles/frontend/public/js", webdir: "/js" },
     { fsdir: "/bundles/frontend/public/css", webdir: "/css" },
     { fsdir: "/bundles/frontend/public/img", webdir: "/img" },
-    { fsdir: "/bundles/frontend/public/font", webdir: "/font" }
+    { fsdir: "/bundles/frontend/public/font", webdir: "/font" },
+    { fsdir: "/bundles/frontend/public/font", webdir: "/fonts" }
   ]
 }
