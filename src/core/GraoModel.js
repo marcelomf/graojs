7edcb97899f0812c280bb73326b6041d734656d1
Marcelo M. Fleury <marcelomf@gmail.com>
2017-08-19 12:24:26 -0300

    Add new param to mongoose connection

--- src/core/GraoModel.js
+++ src/core/GraoModel.js
@@ -1,6 +1,7 @@
 var GraoModel = function(di) {
   di.event.newSuccess('Database Connection....');
-  di.mongoose.connect(di.config.db);
+  //di.mongoose.connect(di.config.db);
+  di.mongoose.connect(di.config.db, {useMongoClient: true});
   di.event.newSuccess('Instance created');
   di.models = this;
   di.models = di.loader.tryLoad(di.loader.loading('model'), di, 'models');
