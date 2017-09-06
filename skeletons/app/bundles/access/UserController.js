4d5e4e9eaaae7207c45965106e3506b01c03fa2b
Marcelo M. Fleury <marcelomf@gmail.com>
2017-09-06 11:05:19 -0300

    User arrow functions and async/await in UserController

--- skeletons/app/bundles/access/UserController.js
+++ skeletons/app/bundles/access/UserController.js
@@ -1,55 +1,55 @@
 var service = {}, admin = {}, helper = {}, models, controllers, event, config, User, $i;
 
-service.count = function(req, res) {
-  var dataList = controllers.processDataList(User, req.query);
-
-  User.count({}, function(err, totality) {
-    if(err) {
-      res.json(event.newError(err).toJson());
-      return;
-    } 
-
+service.count = async (req, res) => {
+  try {
+    let dataList = controllers.processDataList(User, req.query);
+    let totality = await User.count();
+    
     if(dataList.filter == null) {
       res.json({totality: totality, filtered: 0});
       return;
     }
-
-    User.count(dataList.filter, function(err, filtered) {
-      if(err)
-        res.json(event.newError(err).toJson());
-      else
-        res.json({totality: totality, filtered: filtered});
-    });
-  });
+    
+    let filtered = await User.count(dataList.filter);
+    res.json({totality: totality, filtered: filtered});
+    return;
+  } catch(err) {
+    res.json(event.newError(err).toJson());
+    return;
+  }
 }
 
-service.get = function(req, res) {
-    User.findOne({_id : req.params.id}, "-password").populate('activitys').exec(function(err, user) {
-    if (err)
-      res.json(event.newError(err).toJson());
-    else
-      res.json(user);
-  });
+service.get = async (req, res) => {
+  try {
+    let user = await User.findOne({_id : req.params.id}, "-password").
+                      populate('activitys').exec(); 
+    res.json(user);
+    return;
+  } catch(err) {
+    res.json(event.newError(err).toJson());
+    return;
+  }        
 }
 
-service.query = function(req, res) {
-  var dataList = controllers.processDataList(User, req.query);
+service.query = async (req, res) => {
+  try {
+    var dataList = controllers.processDataList(User, req.query);
 
-  User.find(dataList.filter, "-password").
-    sort(dataList.sort).
-    skip(dataList.page.skip).
-    limit(dataList.page.limit).
-    populate('user').
-    populate('activitys').
-    exec(function(err, users) {
-      if(err)
-        res.json(event.newError(err).toJson());
-      else
-        res.json(users);
-  });
+    let users = await User.find(dataList.filter, "-password").
+      sort(dataList.sort).
+      skip(dataList.page.skip).
+      limit(dataList.page.limit).
+      populate('user').
+      populate('activitys').
+      exec();
+    return res.json(users);  
+  } catch(err) {
+    res.json(event.newError(err).toJson());
+    return;
+  }
 }
 
-service.validate = function(req, res, next) {
+service.validate = (req, res, next) => {
   req.body.password = 'byPass12321';
   var user = new User(req.body);
   user.validate(function(err){
@@ -60,7 +60,7 @@ service.validate = function(req, res, next) {
   });
 }
 
-helper.fixError = function(err) {
+helper.fixError = (err) => {
   if(err) {
     if(err.errors && err.errors && err.errors.password) {
       err.errors.newPassword = err.errors.password;
@@ -70,7 +70,7 @@ helper.fixError = function(err) {
   return err;
 }
 
-service.create = function(req, res) {
+service.create = (req, res) => {
   if(req.body.newPassword != req.body.confirmNewPassword)
     return res.json(event.newError(res.__('Error in the password confirmation.')).toJson());
 
@@ -94,7 +94,7 @@ service.create = function(req, res) {
   });
 }
 
-service.update = function(req, res) {
+service.update = (req, res) => {
   function save(userJson){
     var userId = userJson._id;
     delete userJson._id;
@@ -134,7 +134,7 @@ service.update = function(req, res) {
   });
 }
 
-service.updateProfile = function(req, res) {
+service.updateProfile = (req, res) => {
 
   function save(user) {
     delete req.body.password;
@@ -184,7 +184,7 @@ service.updateProfile = function(req, res) {
   }
 }
 
-service.destroy = function(req, res) {  
+service.destroy = (req, res) => {  
   User.remove({_id : req.params.id}, function(err) {
     if(err)
       res.json(event.newError(err).toJson());
@@ -193,14 +193,14 @@ service.destroy = function(req, res) {
   });
 }
 
-admin.dashboard = function(req, res) {
+admin.dashboard = (req, res) => {
   var locale = (config.locales.indexOf(req.cookies.locale) >= 0) ? req.cookies.locale : config.defaultLocale;
   res.render('access/view/user_dashboard', { isAuth: req.isAuthenticated(), 
                                              locale: locale, 
                                              user: req.user });
 }
 
-admin.profile = function(req, res) {
+admin.profile = (req, res) => {
   var locale = (config.locales.indexOf(req.cookies.locale) >= 0) ? req.cookies.locale : config.defaultLocale;
   res.render('access/view/user_profile', { isAuth: req.isAuthenticated(), 
                                              locale: locale, 
