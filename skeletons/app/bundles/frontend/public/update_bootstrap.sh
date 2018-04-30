7bf9ec43d88049d454c13ed9e4a461436e351b6d
Marcelo M. Fleury <marcelomf@gmail.com>
2018-04-30 13:49:31 -0300

    Update css

--- skeletons/app/bundles/frontend/public/update_bootstrap.sh
+++ skeletons/app/bundles/frontend/public/update_bootstrap.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
-cd public/css
 rm *.css
 
 wget https://raw.github.com/synackbr/graojs-visual/master/theme/graojs/bootstrap.min.css -O graojs.min.css
-wget http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.csss -O bootstrap.min.css
+wget http://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css -O bootstrap.min.css
 
-for theme in amelia cerulean cosmo cyborg flatly journal readable simplex slate spacelab united
+for theme in cerulean cosmo cyborg flatly journal readable simplex slate spacelab united darkly lumen paper sandstone superhero yeti
 do
-    wget http://netdna.bootstrapcdn.com/bootswatch/3.0.0/${theme}/bootstrap.min.css -O ${theme}.min.css
+    wget http://netdna.bootstrapcdn.com/bootswatch/3.3.7/${theme}/bootstrap.min.css -O ${theme}.min.css
 done
